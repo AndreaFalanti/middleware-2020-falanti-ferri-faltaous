d5ab6a1f1c3b01359655d7409cc01024dd8fc60c
AndreaFalanti <a.falanti@gmail.com>
2021-02-15 00:17:19 +0100

    Distribute week records into 7 rows (days)
    Also refactor code a bit, improve format and add more comments.

--- project5/src/main/java/it/polimi/falanti_ferri_faltaous/project5/Main.java
+++ project5/src/main/java/it/polimi/falanti_ferri_faltaous/project5/Main.java
@@ -1,42 +1,40 @@
 package it.polimi.falanti_ferri_faltaous.project5;
 
 import it.polimi.falanti_ferri_faltaous.project5.utils.LogUtils;
+import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.expressions.Window;
 import org.apache.spark.sql.expressions.WindowSpec;
+import org.apache.spark.sql.types.DataTypes;
 
 import static org.apache.spark.sql.functions.*;
 
 
 public class Main {
-//    private static Row[] expandInDays(Row line) {
-//        Row[] rows = new Row[7];
-//
-//        for (int i = 0; i < 7; i++) {
-//            rows[i] = RowFactory.create(line.getDate(0), line.getLong(1), line.getString(2));
-//        }
-//        return rows;
-//    }
+    /**
+     * Distribute weekly cases among days as integer values, putting the rest on farther days
+     * @param day 0-indexed value
+     * @return SQL Column with daily count
+     */
+    private static Column computeDayCases(int day) {
+        return col("cases_weekly").$div(7).$plus(day / 7.0).cast(DataTypes.LongType);
+    }
+
+    /**
+     * Compute date for row split from week record
+     * @param day 0-indexed value
+     * @return SQL Column with daily count
+     */
+    private static Column computeDate(int day) {
+        return date_sub(col("date"), 6 - day);
+    }
 
     public static void main(String[] args) {
         LogUtils.setLogLevel();
 
         final String master = args.length > 0 ? args[0] : "local[4]";
-
-//        final List<StructField> mySchemaFields = new ArrayList<>();
-//        mySchemaFields.add(DataTypes.createStructField("dateRep", DataTypes.DateType, false));
-//        mySchemaFields.add(DataTypes.createStructField("year_week", DataTypes.StringType, false));
-//        mySchemaFields.add(DataTypes.createStructField("cases_weekly", DataTypes.LongType, false));
-//        mySchemaFields.add(DataTypes.createStructField("deaths_weekly", DataTypes.LongType, false));
-//        mySchemaFields.add(DataTypes.createStructField("countriesAndTerritories", DataTypes.StringType, false));
-//        mySchemaFields.add(DataTypes.createStructField("geoId", DataTypes.StringType, false));
-//        mySchemaFields.add(DataTypes.createStructField("countryterritoryCode", DataTypes.StringType, false));
-//        mySchemaFields.add(DataTypes.createStructField("popData2019", DataTypes.LongType, false));
-//        mySchemaFields.add(DataTypes.createStructField("continentExp", DataTypes.StringType, false));
-//        mySchemaFields.add(DataTypes.createStructField("notification_rate_per_100000_population_14-days", DataTypes.FloatType, false));
-//        final StructType mySchema = DataTypes.createStructType(mySchemaFields);
 
         final SparkSession spark = SparkSession
                 .builder()
@@ -44,35 +42,56 @@ public class Main {
                 .appName("Project-5")
                 .getOrCreate();
 
+        // READ INPUT + PREPROCESSING
         // create the dataframe, directly infer the structure from the csv headers
         // headers: dateRep, year_week, cases_weekly, deaths_weekly, countriesAndTerritories, geoId,
         // countryterritoryCode, popData2019, continentExp, notification_rate_per_100000_population_14-days
         // Also cast the date to correct type, so that we can order the rows later. Take only the columns required for
         // the project operations.
+        // Divide each rows in 7 rows -> from weeks to days
         final Dataset<Row> coronaRecords = spark
                 .read()
                 .option("header", "true")
                 .option("delimiter", ",")
                 .csv("data/data.csv")
                 .withColumn("date", to_date(col("dateRep"), "dd/MM/yyyy"))
-                .select("date", "cases_weekly", "countriesAndTerritories");
+                .withColumn("cases_day1", computeDayCases(0))
+                .withColumn("date_day1", computeDate(0))
+                .withColumn("cases_day2", computeDayCases(1))
+                .withColumn("date_day2", computeDate(1))
+                .withColumn("cases_day3", computeDayCases(2))
+                .withColumn("date_day3", computeDate(2))
+                .withColumn("cases_day4", computeDayCases(3))
+                .withColumn("date_day4", computeDate(3))
+                .withColumn("cases_day5", computeDayCases(4))
+                .withColumn("date_day5", computeDate(4))
+                .withColumn("cases_day6", computeDayCases(5))
+                .withColumn("date_day6", computeDate(5))
+                .withColumn("cases_day7", computeDayCases(6))
+                .withColumn("date_day7", computeDate(6))
+                .withColumn("zip_col", explode(arrays_zip(
+                        array("cases_day1", "cases_day2", "cases_day3", "cases_day4",
+                        "cases_day5", "cases_day6", "cases_day7"), array("date_day1", "date_day2",
+                        "date_day3", "date_day4", "date_day5", "date_day6", "date_day7"))))
+                .select(col("zip_col").getItem("1").as("date"), col("zip_col").getItem("0").as("cases"),
+                        col("countriesAndTerritories"))
+                .orderBy("date");
 
         // DEBUG: test that the dataset works fine and the structure is correct
-        // coronaRecords.filter(col("countriesAndTerritories").equalTo("Albania")).show();
+        coronaRecords.filter(col("countriesAndTerritories").equalTo("Albania")).show();
+        coronaRecords.printSchema();
 
-//        coronaRecords = coronaRecords.flatMap(
-//                (FlatMapFunction<Row, Row>) line -> Arrays.asList(expandInDays(line)).iterator(),
-//                Encoders.tuple(Encoders.DATE(), Encoders.LONG(), Encoders.STRING())
-//        );
 
-        final Dataset<Row> movAvg = coronaRecords.withColumn("movingAverage", avg(col("cases_weekly"))
-                .over(Window.partitionBy("countriesAndTerritories").orderBy(col("date")).rowsBetween(-6, 0)));
-
-        movAvg.show(100, false);
-        //movAvg.printSchema();
-
+        // 1st OPERATION: compute the moving average
         WindowSpec windowSpec = Window.partitionBy("countriesAndTerritories").orderBy(col("date"));
 
+        final Dataset<Row> movAvg = coronaRecords.withColumn("movingAverage", avg(col("cases"))
+                .over(windowSpec.rowsBetween(-6, 0)));
+
+        movAvg.show(100, false);
+
+
+        // 2nd OPERATION: compute percentage variation between the average of the day and the one of the previous day
         final Dataset<Row> percentageIncrease = movAvg
             .withColumn("variation",
                 when((lag("movingAverage", 1).over(windowSpec)).isNull(), 0)
@@ -87,23 +106,8 @@ public class Main {
 
         percentageIncrease.show(100, false);
 
-//        final Dataset<Row> maxIncreasePerDay = percentageIncrease
-//                .groupBy("date")
-//                .max("variationPercentage")
-//                .orderBy("date");
-//
-//        maxIncreasePerDay.show();
-//
-//        final Dataset<Row> topCountriesForDay = percentageIncrease
-//                .join(maxIncreasePerDay,
-//                    percentageIncrease.col("variationPercentage").equalTo(maxIncreasePerDay.col("max(variationPercentage)"))
-//                            .and(percentageIncrease.col("date").equalTo(maxIncreasePerDay.col("date"))),
-//                    "leftsemi")
-//                .where(col("date").$greater(new Date(120, 1, 1)))
-//                .orderBy("date");
-//
-//        topCountriesForDay.show();
 
+        //3rd OPERATION: top 10 countries with the greatest percentage increase per day
         WindowSpec windowSpec2 = Window.partitionBy("date").orderBy(col("variationPercentage").desc());
 
         final Dataset<Row> top10CountriesPerDay = percentageIncrease
@@ -112,6 +116,7 @@ public class Main {
                 .orderBy("date", "top");
 
         top10CountriesPerDay.show(100, false);
+        //top10CountriesPerDay.where(col("date").$greater(new Date(120, 1, 1))).show(100, false);
 
         spark.close();
     }
